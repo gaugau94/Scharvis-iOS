51e7363f9ac331cd17e681cb072e5e53b7dc6089
Gaultier Moraillon <gaultiermoraillon@gmail.com>
2016-07-11 17:50:40 +0200

    First finish version

--- Login/Action_Page.swift
+++ Login/Action_Page.swift
@@ -57,9 +57,10 @@ class Action_Page: UIViewController {
             let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
             self.string = responseString as! String
             print("Piece contient \(self.string)")
+            dispatch_async(dispatch_get_main_queue(), {
             self.addToArray()
             self.goToStatus()
-            
+            })
         }
         task.resume()
     }
@@ -89,11 +90,11 @@ class Action_Page: UIViewController {
             idStatus = Int(string4)!
             print(idStatus)
             print(string6)
-            getStatus(string6)
+            self.getStatus(self.string6, idStatus: self.idStatus)
             //createLabel(string6)
-            string3 = ""
-            string4 = ""
-            string6 = ""
+            self.string3 = ""
+            self.string4 = ""
+            self.string6 = ""
             i = i + 1
         }
     }
@@ -103,7 +104,6 @@ class Action_Page: UIViewController {
         print(string6)
         let label: UILabel = UILabel()
         label.frame = CGRectMake(x, y, 200, 21)
-        //label.backgroundColor = UIColor.blackColor()
         label.textColor = UIColor.blackColor()
         label.textAlignment = NSTextAlignment.Center
         label.text = string6
@@ -112,6 +112,8 @@ class Action_Page: UIViewController {
     
     func createSwitch(string2: String)
     {
+        
+        
         let switchDemo = UISwitch(frame:CGRectMake(200, y, 0, 0));
         if (string2 == "0" || string2 == "00")
         {
@@ -120,25 +122,66 @@ class Action_Page: UIViewController {
         else {
             switchDemo.on = false
         }
-        //switchDemo.setOn(true, animated: false);
         switchDemo.addTarget(self, action: #selector(Action_Page.switchValueDidChange(_:)), forControlEvents: .ValueChanged);
-        switchDemo.tag = Int(string6)!
+        switchDemo.tag = idStatus
         self.view.addSubview(switchDemo);
     }
     
     func switchValueDidChange(sender:UISwitch!)
     {
+            let user = "0658092965"
+        print(user)
+        let idaction = sender.tag
+        
         if (sender.on == true){
             print("on")
             print(sender.tag)
+            let myUrl1 = NSURL(string : "http://163.5.84.234:4567/action?action=0&id=\(idaction)&username=\(user)")
+            print(myUrl1)
+            let request = NSMutableURLRequest(URL:myUrl1!);
+            request.HTTPMethod = "POST";
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request)
+            {
+                data, response, error in
+                
+                if error != nil
+                {
+                    print("error=\(error)")
+                    return
+                }
+                print("API Responce \(response)")
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                self.string2 = responseString as! String
+            }
+            task.resume()
+
         }
         else{
             print("off")
             print(sender.tag)
+            let myUrl = NSURL(string : "http://163.5.84.234:4567/action?action=1&id=\(idaction)&username=\(user)")
+            print(user)
+            print(myUrl)
+            let request = NSMutableURLRequest(URL:myUrl!);
+            request.HTTPMethod = "POST";
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request)
+            {
+                data, response, error in
+                
+                if error != nil
+                {
+                    print("error=\(error)")
+                    return
+                }
+                print("API Responce \(response)")
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                self.string2 = responseString as! String
+            }
+            task.resume()
         }
     }
 
-    func getStatus(string6: String)
+    func getStatus(string6: String, idStatus: Int)
     {
         let myUrl = NSURL(string : "http://163.5.84.234:4567/status?id=\(idStatus)")
         let request = NSMutableURLRequest(URL:myUrl!);
@@ -156,7 +199,9 @@ class Action_Page: UIViewController {
             let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
             self.string2 = responseString as! String
             print("Piece status \(self.string2)")
+            print("Piece ID \(self.idStatus)")
             dispatch_async(dispatch_get_main_queue(), {
+                print("Piece IDDDDDDDDD \(self.idStatus)")
             self.createLabel(string6)
             self.createSwitch(self.string2)
             self.y = self.y + 50
